ab971814495b9024a1306e45e98c1a39bd596170
tiogars <tiogars@gmail.com>
2025-12-14 09:20:34 +0100

    docs: add missing class-level documentation for PageDefinitionController

--- src/main/java/fr/tiogars/starter/controllers/PageDefinitionController.java
+++ src/main/java/fr/tiogars/starter/controllers/PageDefinitionController.java
@@ -3,6 +3,9 @@ package fr.tiogars.starter.controllers;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller for managing Page Definitions.
+ */
 @CrossOrigin
 @RestController
 public class PageDefinitionController {
